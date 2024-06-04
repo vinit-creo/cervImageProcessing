538b4d7b58f2153062785866de5addc99237d491
vinit-creo <vinit@creoit.com>
2024-06-04 12:35:31 +0530

    - add code to convert grey image to tensors.

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 
-
-
 img = cv2.imread("assets/image1.jpg")
-
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 grad_x = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=3)
 grad_y = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
 magnitude = cv2.magnitude(grad_x, grad_y)
-direction = cv2.phase(grad_x, grad_y, angleInDegrees=True)
 magnitude = cv2.convertScaleAbs(magnitude)
-cv2.imshow('Gradient Magnitude', magnitude)
-cv2.imshow('Gradient Direction', direction)
 
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+tensor = transform(img_gray) # tensors are printed here
+print(tensor)
 
 plt.subplot(1, 1, 1)
 plt.imshow(img_gray)
