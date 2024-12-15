fbe223f027c04d8e9911f23733d788df75f22b32
vinit <vinit@creoit.com>
2024-12-16 01:58:58 +0530

    - stashing changes

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-
-img = cv2.imread("/Users/vinit/Downloads/experiment_media/image_2.jpg")
+img = cv2.imread("assets/image_1.jpg")
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -12,8 +11,8 @@ grad_y = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
 magnitude = cv2.magnitude(grad_x, grad_y)
 direction = cv2.phase(grad_x, grad_y, angleInDegrees=True)
 magnitude = cv2.convertScaleAbs(magnitude)
-cv2.imshow('Gradient Magnitude', magnitude)
-cv2.imshow('Gradient Direction', direction)
+cv2.imshow("Gradient Magnitude", magnitude)
+cv2.imshow("Gradient Direction", direction)
 
 
 plt.subplot(1, 1, 1)
